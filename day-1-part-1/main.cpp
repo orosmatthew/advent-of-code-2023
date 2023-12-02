9b607c57f73c49ac2c8e733f4a29c3261e77fd4d
orosmatthew <orosmatthew@pm.me>
2023-12-02 12:47:48 -0500

    Optimize day 1 part 1

--- day-1-part-1/main.cpp
+++ day-1-part-1/main.cpp
@@ -3,10 +3,9 @@
 #include <filesystem>
 #include <fstream>
 #include <iostream>
-#include <optional>
 #include <sstream>
 
-std::string read_data(const std::filesystem::path& path)
+static std::string read_data(const std::filesystem::path& path)
 {
     std::fstream in_file(path);
     std::stringstream ss;
@@ -14,25 +13,40 @@ std::string read_data(const std::filesystem::path& path)
     return ss.str();
 }
 
-int solve(const std::string& data)
+static int solve(const std::string& data)
 {
     int total = 0;
-    std::optional<char> first;
-    std::optional<char> last;
-    for (const char c : data) {
-        if (std::isdigit(c)) {
-            if (!first.has_value()) {
-                first = c;
+
+    for (int i = 0; i < data.size(); ++i) {
+        int first = -1;
+        int forward_idx = i;
+        for (; forward_idx < data.size(); ++forward_idx) {
+            if (data[forward_idx] >= '0' && data[forward_idx] <= '9') {
+                first = data[forward_idx] - '0';
+                break;
             }
-            last = c;
         }
-        else if (c == '\n') {
-            assert(first.has_value() && last.has_value() && "There must be at least one number");
-            std::array num_str = { first.value(), last.value(), '\0' };
-            total += std::stoi(num_str.data());
-            first.reset();
-            last.reset();
+    first_done:
+        int reverse_idx = forward_idx;
+        int next_idx = static_cast<int>(data.size()) - 1;
+        for (; reverse_idx < data.size(); ++reverse_idx) {
+            if (data[reverse_idx] == '\n') {
+                next_idx = reverse_idx;
+                reverse_idx--;
+                break;
+            }
         }
+        int last = -1;
+        for (; reverse_idx >= forward_idx; --reverse_idx) {
+            if (data[reverse_idx] >= '0' && data[reverse_idx] <= '9') {
+                last = data[reverse_idx] - '0';
+                break;
+            }
+        }
+    last_done:
+        assert(first != -1 && last != -1 && "There must be at least one number");
+        total += 10 * first + last;
+        i = next_idx;
     }
 
     return total;
@@ -45,12 +59,15 @@ int main()
     const std::string data = read_data("./day-1-part-1/input.txt");
 
 #ifdef BENCHMARK
-    constexpr int n_runs = 10000;
+    constexpr int n_runs = 100000;
     double time_running_total = 0.0;
 
     for (int n_run = 0; n_run < n_runs; ++n_run) {
         auto start = std::chrono::high_resolution_clock::now();
-        solve(data);
+        // ReSharper disable once CppDFAUnusedValue
+        // ReSharper disable once CppDFAUnreadVariable
+        // ReSharper disable once CppDeclaratorNeverUsed
+        volatile int result = solve(data);
         auto end = std::chrono::high_resolution_clock::now();
         time_running_total += std::chrono::duration<double, std::nano>(end - start).count();
     }
