c2d27e16439b280f750ef05247180c93a258068d
orosmatthew <orosmatthew@pm.me>
2023-12-01 23:34:14 -0500

    Optimize code

--- day-1-part-2/main.cpp
+++ day-1-part-2/main.cpp
@@ -4,9 +4,8 @@
 #include <iostream>
 #include <optional>
 #include <sstream>
-#include <vector>
 
-std::string read_data(const std::filesystem::path& path)
+static std::string read_data(const std::filesystem::path& path)
 {
     std::fstream in_file(path);
     std::stringstream ss;
@@ -14,69 +13,107 @@ std::string read_data(const std::filesystem::path& path)
     return ss.str();
 }
 
+static bool cmp_str(const char* a, const char* b, const int n)
+{
+    for (int i = 0; i < n; ++i) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+        if (a[i] == '\0' && b[i] == '\0') {
+            break;
+        }
+    }
+    return true;
+}
+
 struct DigitStr {
     const char* str;
     int length;
-    char value;
+    int value;
 };
 
-int solve(const std::string& data)
+static int solve(const std::string& data)
 {
     int total = 0;
 
     std::array<DigitStr, 9> digit_strs
-        = { { { "one", 3, '1' },
-              { "two", 3, '2' },
-              { "three", 5, '3' },
-              { "four", 4, '4' },
-              { "five", 4, '5' },
-              { "six", 3, '6' },
-              { "seven", 5, '7' },
-              { "eight", 5, '8' },
-              { "nine", 4, '9' } } };
+        = { { { "one", 3, 1 },
+              { "two", 3, 2 },
+              { "three", 5, 3 },
+              { "four", 4, 4 },
+              { "five", 4, 5 },
+              { "six", 3, 6 },
+              { "seven", 5, 7 },
+              { "eight", 5, 8 },
+              { "nine", 4, 9 } } };
 
-    std::optional<char> first;
-    std::optional<char> last;
-    for (const char& c : data) {
-        if (std::isdigit(c)) {
-            if (!first.has_value()) {
-                first = c;
+    for (int i = 0; i < data.size(); ++i) {
+        std::optional<int> first;
+        int forward_idx = i;
+        for (; forward_idx < data.size(); ++forward_idx) {
+            if (data[forward_idx] >= '0' && data[forward_idx] <= '9') {
+                first = data[forward_idx] - '0';
+                break;
             }
-            last = c;
-        }
-        else if (std::isalpha(c)) {
-            for (const auto [str, length, value] : digit_strs) {
-                if (std::strncmp(&c, str, length) == 0) {
-                    if (!first.has_value()) {
+            if (data[forward_idx] >= 'a' && data[forward_idx] <= 'z') {
+                for (const auto [str, length, value] : digit_strs) {
+                    if (cmp_str(&data[forward_idx], str, length)) {
                         first = value;
+                        goto first_done;
                     }
-                    last = value;
-                    break;
                 }
             }
         }
-        else if (c == '\n') {
-            assert(first.has_value() && last.has_value() && "There must be at least one number");
-            std::array num_str = { first.value(), last.value(), '\0' };
-            total += std::stoi(num_str.data());
-            first.reset();
-            last.reset();
+    first_done:
+        int reverse_idx = forward_idx;
+        int next_idx = static_cast<int>(data.size()) - 1;
+        for (; reverse_idx < data.size(); ++reverse_idx) {
+            if (data[reverse_idx] == '\n') {
+                next_idx = reverse_idx;
+                reverse_idx--;
+                break;
+            }
         }
+        std::optional<int> last;
+        for (; reverse_idx >= forward_idx; --reverse_idx) {
+            if (data[reverse_idx] >= '0' && data[reverse_idx] <= '9') {
+                last = data[reverse_idx] - '0';
+                break;
+            }
+            if (data[reverse_idx] >= 'a' && data[reverse_idx] <= 'z') {
+                for (const auto [str, length, value] : digit_strs) {
+                    if (cmp_str(&data[reverse_idx], str, length)) {
+                        last = value;
+                        goto last_done;
+                    }
+                }
+            }
+        }
+    last_done:
+        assert(first.has_value() && last.has_value() && "There must be at least one number");
+        total += 10 * first.value() + last.value();
+        i = next_idx;
     }
+
     return total;
 }
+
+// #define BENCHMARK
 
 int main()
 {
     const std::string data = read_data("./day-1-part-2/input.txt");
 
 #ifdef BENCHMARK
-    constexpr int n_runs = 10000;
+    constexpr int n_runs = 100000;
     double time_running_total = 0.0;
 
     for (int n_run = 0; n_run < n_runs; ++n_run) {
         auto start = std::chrono::high_resolution_clock::now();
-        solve(data);
+        // ReSharper disable once CppDFAUnusedValue
+        // ReSharper disable once CppDFAUnreadVariable
+        // ReSharper disable once CppDeclaratorNeverUsed
+        volatile int result = solve(data);
         auto end = std::chrono::high_resolution_clock::now();
         time_running_total += std::chrono::duration<double, std::nano>(end - start).count();
     }
